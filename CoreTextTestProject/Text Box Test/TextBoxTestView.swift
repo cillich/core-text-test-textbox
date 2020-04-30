78d6fb6e8665d6a65d9900aabb541427de57f5e6
Claire Illich <claire@gingerlabs.com>
2020-04-29 18:17:24 -0700

    Added different ways of laying out the text lines in order to determine what does and does not work for aligning text

--- CoreTextTestProject/Text Box Test/TextBoxTestView.swift
+++ CoreTextTestProject/Text Box Test/TextBoxTestView.swift
@@ -31,18 +31,62 @@ class TextBoxTestView: UIView {
         context.textMatrix = .identity
         context.translateBy(x: 0, y: bounds.size.height)
         context.scaleBy(x: 1.0, y: -1.0)
-        //
-        let path = CGMutablePath()
-        // The rect is the bounds with insets for margins along the edges of the bounds
-        let frameRect = CGRect(origin: bounds.origin, size: bounds.size).insetBy(dx: 10.0, dy: 10.0)
-        path.addRect(frameRect)
-
-        let framesetter = CTFramesetterCreateWithAttributedString(attrString as CFAttributedString)
-
-        let frame = CTFramesetterCreateFrame(framesetter, CFRangeMake(0, attrString.length), path, nil)
-
-        CTFrameDraw(frame, context)
         
+// ---------------------------------------------------------------------------------------------------------
+        // MARK: Option 1: Using Framesetter - Works!
+        
+//        let path = CGMutablePath()
+//        // The rect is the bounds with insets for margins along the edges of the bounds
+//        let frameRect = CGRect(origin: bounds.origin, size: bounds.size).insetBy(dx: 10.0, dy: 10.0)
+//        path.addRect(frameRect)
+//
+//        let framesetter = CTFramesetterCreateWithAttributedString(attrString as CFAttributedString)
+//
+//        let frame = CTFramesetterCreateFrame(framesetter, CFRangeMake(0, attrString.length), path, nil)
+//
+//        CTFrameDraw(frame, context)
+        
+// ---------------------------------------------------------------------------------------------------------
+        // MARK: Option 2: Manually Calculating Line Breaks - Does not work with alignment!
+        
+//        let typesetter = CTTypesetterCreateWithAttributedString(attrString)
+//        let availableWidth: Double = Double(bounds.size.width) - 10.0
+//        var start: CFIndex = 0
+//        var lineNumber: CGFloat = 1.0
+//
+//        while start < attrString.length {
+//            let count: CFIndex = CTTypesetterSuggestLineBreak(typesetter, start, availableWidth)
+//
+//            let line: CTLine = CTTypesetterCreateLine(typesetter, CFRangeMake(start, count))
+//            context.textPosition = CGPoint(x: 5, y: bounds.size.height - (30.0 * lineNumber)) // 30 is hardcoded
+//            CTLineDraw(line, context)
+//            start += count
+//            lineNumber += 1
+//        }
+        
+// ---------------------------------------------------------------------------------------------------------
+        // MARK: Option 3: Manually Calculating Line Breaks With Flush(?) - Works!
+        
+        let typesetter = CTTypesetterCreateWithAttributedString(attrString)
+        let availableWidth: Double = Double(bounds.size.width) - 10.0 // 10 leaves 5 points of margin on both sides
+        var start: CFIndex = 0
+        var lineNumber: CGFloat = 1.0
+        
+        while start < attrString.length {
+            let count: CFIndex = CTTypesetterSuggestLineBreak(typesetter, start, availableWidth)
+            
+            let line: CTLine = CTTypesetterCreateLine(typesetter, CFRangeMake(start, count))
+            
+            // Alignment stuff?
+            let flush: CGFloat = 0.5 // centered
+            let penOffset: CGFloat = CGFloat(CTLineGetPenOffsetForFlush(line, flush, availableWidth))
+            
+            context.textPosition = CGPoint(x: 5 + penOffset, y: bounds.size.height - (30.0 * lineNumber)) // 30 is hardcoded
+            CTLineDraw(line, context)
+            start += count
+            lineNumber += 1
+        }
+
     }
     
     func changeTextAlignment(alignment: NSTextAlignment) {
